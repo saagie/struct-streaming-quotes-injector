e1baa90ef6046d3e6137c47b9a29e69e96c49a19
Aurélien Vandel <aurelien@creativedata.fr>
2017-04-18 22:51:20 +0200

    branch with part to complete

--- src/main/scala/io/saagie/quotes/QuotesVariation.scala
+++ src/main/scala/io/saagie/quotes/QuotesVariation.scala
@@ -1,6 +1,5 @@
 package io.saagie.quotes
 
-import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.{OutputMode, ProcessingTime}
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
@@ -22,12 +21,9 @@ object QuotesVariation extends SparkLauncher {
     * @return The stream's DataFrame.
     */
   private def readStream(): DataFrame = {
-    spark
-      .readStream
-      .format("kafka") // From Kafka
-      .option("kafka.bootstrap.servers", params.kafkaBrokerUrl) // Broker URL
-      .option("subscribe", params.kafkaTopic) // Topic name
-      .load()
+    spark.readStream
+     ???
+
   }
 
   /**
@@ -55,16 +51,11 @@ object QuotesVariation extends SparkLauncher {
     // Create Dataset
     val dataset = readStream().selectExpr("CAST(value AS STRING)")
       .as[String]
-      .flatMap(Quote.parse(_) match {
-        case Some(q) => Some(q)
-        case _ => None
-      })
+      ???
 
     // Compute evolution
     val meanPrice = dataset.withColumn("timestamp", $"ts".cast("timestamp"))
-      .withWatermark("timestamp", "2 minutes")
-      .groupBy(window($"timestamp", "2 minutes") as 'time, $"symbol" as 'label)
-      .agg(avg("price").as('avgPrice))
+     ???
 
     // Start running the query and write the result into a file
     writeStream(meanPrice)
